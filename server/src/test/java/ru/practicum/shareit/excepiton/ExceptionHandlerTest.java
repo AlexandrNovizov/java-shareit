bee2605614958b94d3fe57218ff0ec74f8f2b4e7
AlexandrNovizov <126908706+AlexandrNovizov@users.noreply.github.com>
2026-06-07 18:51:42 +0400

    added test for ErrorHandler

--- server/src/test/java/ru/practicum/shareit/excepiton/ExceptionHandlerTest.java
+++ server/src/test/java/ru/practicum/shareit/excepiton/ExceptionHandlerTest.java
@@ -3,12 +3,10 @@ package ru.practicum.shareit.excepiton;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import ru.practicum.shareit.exception.AccessDeniedException;
@@ -29,7 +27,6 @@ import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest({UserController.class, ItemController.class})
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class ExceptionHandlerTest {
